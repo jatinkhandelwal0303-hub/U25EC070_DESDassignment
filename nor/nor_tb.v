2d5a814d79fcf9643f2687e8171f253b0c8db970
jatinkhandelwal0303-hub <jatinkhandelwal0303@gmail.com>
2026-06-05 16:28:43 +0530

    Add files via upload

---
+++ nor/nor_tb.v
@@ -0,0 +1,20 @@
+module nor_gate_tb ;
+reg a;
+reg b;
+wire y;
+
+nor_gate uut(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    a = 0 ; b = 0 ;
+    #10 a = 0 ; b = 1;
+    #10 a = 1 ; b = 0;
+    #10 a = 1 ; b = 1;
+    #20 $finish ;
+end
+
+endmodule
